033e3966739f2f43d9ead8309e815a4454c54830
arthur <tehtaq@gmail.com>
2024-02-01 23:43:58 +0100

    args instead of asset

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use core::time;
+use std::process::exit;
 use std::thread::sleep;
 use std::path::Path;
 use std::env;
@@ -6,27 +7,47 @@ use std::env;
 use wallpaper;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        eprintln!("Invalid arguments");
+        eprintln!("Usage: ./dimmer <dimmer_img_path> <process_string>");
+        exit(1);
+    }
+
+    let path_dimmer = &args[1];
+    let process_string = &args[2];
+
     // First, get the current background path so that we can reset it when this program ends
+    // let cl = path_user_wp.clone();
     let path_user_wp = wallpaper::get().unwrap();
-    let path_cur = env::current_dir().unwrap();
-    let path_new_wp = format!("{}/black.jpg", path_cur.display());
+
+    // ctrl-c handler
+    ctrlc::set_handler({
+        let path_user_wp_clone = path_user_wp.clone();
+        move || {
+            println!("Resetting wallpaper...");
+            wallpaper::set_from_path(&path_user_wp_clone).unwrap();
+            exit(1);
+        }
+    }).unwrap();
 
     println!("User wallpaper: {}", path_user_wp);
-    println!("new wallpaper: {}", path_new_wp);
+    println!("new wallpaper: {}", path_dimmer);
 
-    if !Path::new(&path_new_wp).exists() {
-        panic!("Wallpaper not found ({})", path_new_wp);
+    if !Path::new(&path_dimmer).exists() {
+        panic!("Wallpaper not found ({})", path_dimmer);
     }
 
     // init system
     let mut system = sysinfo::System::new();
-    let mut is_active = false;
 
+    let mut is_active = false;
     loop {
         system.refresh_processes();
-        if system.processes_by_name("League of").collect::<Vec<_>>().len() > 0 {
+        if system.processes_by_name(process_string).collect::<Vec<_>>().len() > 0 {
             if !is_active {
-                wallpaper::set_from_path(&path_new_wp).unwrap();
+                wallpaper::set_from_path(&path_dimmer).unwrap();
                 is_active = true;
                 println!("Dimmer active");
             }
@@ -38,7 +59,7 @@ fn main() {
                 println!("Dimmer inactive");
             }
         }
-        sleep(time::Duration::from_secs(2));
+        sleep(time::Duration::from_millis(1000));
     }
 
 }
